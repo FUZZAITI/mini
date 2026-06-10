258413f55f8762a5347210d9d2bc30c911be6fe3
PEDRO FUZAITI <pedrofuzaitti@gmail.com>
2026-06-10 20:18:09 -0300

    cmd

--- includes/minishell.h
+++ includes/minishell.h
@@ -22,7 +22,7 @@ typedef enum e_type {
 
 typedef struct s_token {
     char            *content;
-    char           *type;
+    t_type           type;
     struct s_token  *next;
 } t_token;
 
@@ -40,26 +40,32 @@ typedef struct s_cmd
     char            *outfile;
     int             append;
     int             heredoc;
+    char            *delimiter;
     struct s_cmd    *next;
 } t_cmd;
 
 void add_list(t_token **list,t_token *token);
-void add_token(t_token **list, char *word, char *type);
+void add_token(t_token **list, char *word, int type);
 int handle_word(t_token **list, char *str);
 int handle_redirect(t_token **tokens, char *str);
 int find_quote_end(char *str, char quote);
 t_token *lexer(char *input);
 t_token	*ft_lstlast(t_token *lst);
+void print_tokens(t_token *list);
 
-int is_redir(char *type);
+
+int is_redir(t_type type);
 int quotes_closed(char *line);
 int check_syntax(t_token *tok, char *line);
 void remove_quotes_tokens(t_token *lst);
 int len_quotes(char *line);
 char *remove_quote(char *token);
 
-t_cmd *parse(t_token *tok);
-new_cmd();
+void handle_cmd_redirect(t_cmd *cmd, t_token *tok);
+t_cmd *new_cmd(int count);
+int count_args(t_token *tok);
+
+
 
 
 
